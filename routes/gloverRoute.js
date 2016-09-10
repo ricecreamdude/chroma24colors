83739edc4de38f210e90a79cebd850d8ff992b24
ricecreamdude <ho.joshua4@gmail.com>
2016-09-10 12:55:25 -0700

    Building up more of gloverRouter

--- routes/gloverRoute.js
+++ routes/gloverRoute.js
@@ -1,35 +1,31 @@
 //Libs
 const express = require('express');
-const app = express();
-
 //Middleware
-var appError = require('appError');
-
+var appError = require( __dirname + '/../lib/appError');
 //Models
 const mongoose = require('mongoose');
 const Glover = require( __dirname + '/../model/gloverSchema');
 
-var gloveRouter = module.exports = exports = express.Router();
+var gloverRouter = module.exports = exports = express.Router();
 
 //Send loading screen info (User profile and top 10 color sets) and favorite color settings
-gloveRouter.route('/')
-
-  .get(req, res) => {
+gloverRouter.route('/')
+  .get( (req, res) => {
     Glover.find( {} , (err, data) => {
       if (err) return appError.error500('gloverRoute - Get: DB Error');
       res.status(200).json(data);
       console.log('Glover GET');
     });
-  }
-  .post(req, res) => {
+  })
+  .post( (req, res) => {
     var newGlover = new Glover(req.body);
     newGlover.save( (err, data) => {
       if (err) return appError.error500('gloverRoute - Post: DB Error');
       res.status(200).json(newGlover);
       console.log('Glover POST');
     });
-  }
-});
+  })
+
 
 // //Create a glove set. Send the created set & confirmation.
 // gloveRouter.post( '', (req, res) => {
